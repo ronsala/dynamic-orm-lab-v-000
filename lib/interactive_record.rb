9ba4f6594c31357a3988a4a8741c5257de1a5598
Ron Sala <ron.sala@gmail.com>
2018-06-10 13:14:53 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,8 +54,8 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end
 
-  # def self.find_by(name: nil, grade: nil)
-  def self.find_by(name: nil, grade:)
+  def self.find_by(name: nil, grade: nil)
+  # def self.find_by(name: nil, grade:) # missing keyword: grade
     sql = <<-SQL
       SELECT * FROM #{self.table_name} WHERE name = '#{name}';
       SELECT * FROM #{self.table_name} WHERE grade = ?
